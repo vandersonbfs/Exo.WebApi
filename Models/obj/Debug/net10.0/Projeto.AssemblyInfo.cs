c0644f91265ab0ecb2b3ce5bedcd22dad73a56ce
vandersonbfs <vanderson_b@hotmail.com>
2025-10-16 19:28:47 -0300

    Criar o restante do CRUD de projetos

--- Models/obj/Debug/net10.0/Projeto.AssemblyInfo.cs
+++ Models/obj/Debug/net10.0/Projeto.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Program")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Program")]
+[assembly: System.Reflection.AssemblyProductAttribute("Projeto")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
--- Repositories/ProjetoRepository.cs
+++ Repositories/ProjetoRepository.cs
@@ -1,5 +1,3 @@
-using Exo.WebApi.Contexts;
-using Exo.WebApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +14,33 @@ _context = context;
 public List<Projeto> Listar()
 {
 return _context.Projetos.ToList();
+}
+// Código novo que completa o CRUD.
+public void Cadastrar(Projeto projeto)
+{
+_context.Projetos.Add(projeto);
+_context.SaveChanges();
+}
+public Projeto BuscarporId(int id)
+{
+return _context.Projetos.Find(id);
+}public void Atualizar(int id, Projeto projeto)
+{
+Projeto projetoBuscado = _context.Projetos.Find(id);
+if (projetoBuscado != null)
+{
+projetoBuscado.NomeDoProjeto = projeto.NomeDoProjeto;
+projetoBuscado.Area = projeto.Area;
+projetoBuscado.Status = projeto.Status;
+}
+_context.Projetos.Update(projetoBuscado);
+_context.SaveChanges();
+}
+public void Deletar(int id)
+{
+Projeto projetoBuscado = _context.Projetos.Find(id);
+_context.Projetos.Remove(projetoBuscado);
+_context.SaveChanges();
 }
 }
 }
